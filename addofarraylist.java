c65910ea614003c9d9e086715022a2ebaed455fa
A.Navaneetha <anavaneethasankar@gmail.com>
2024-12-13 23:10:40 +0530

    Add files via upload

---
+++ addofarraylist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class addofarraylist
+{
+  public static void main(String args[])
+  {
+    ArrayList<Integer> a=new ArrayList<>();
+    a.add(345);
+    a.add(909);
+    a.add(767);
+    System.out.println(a);
+    a.add(1,1517);
+    System.out.println(a);
+    a.add(2,2005);
+    System.out.println(a);
+  } 
+}
